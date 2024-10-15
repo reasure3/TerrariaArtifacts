d77bce43f71c80facbe6472de90bebbdf1c831a9
reasure3 <reasure3@naver.com>
2024-10-15 18:31:15 +0900

    Tinkerers Workshop Data gen 수정

--- src/main/kotlin/com/reasure/terrartifacts/datagen/client/ModBlockStateProvider.kt
+++ src/main/kotlin/com/reasure/terrartifacts/datagen/client/ModBlockStateProvider.kt
@@ -2,7 +2,13 @@ package com.reasure.terrartifacts.datagen.client
 
 import com.reasure.terrartifacts.Terrartifacts
 import com.reasure.terrartifacts.block.ModBlocks
+import com.reasure.terrartifacts.block.TinkerersWorkshopBlock
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.data.PackOutput
+import net.minecraft.util.StringRepresentable
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.properties.Property
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 
@@ -11,5 +17,24 @@ class ModBlockStateProvider(output: PackOutput, exFileHelper: ExistingFileHelper
 
     override fun registerStatesAndModels() {
         simpleBlock(ModBlocks.EXAMPLE_BLOCK)
+
+        horizontalBlockWithProperty(ModBlocks.TINKERERS_WORKSHOP, TinkerersWorkshopBlock.PART)
     }
+
+    private fun <T : Comparable<T>> horizontalBlockWithProperty(
+        block: Block,
+        property: Property<T>,
+        postfix: (T) -> String
+    ) {
+        val name = BuiltInRegistries.BLOCK.getKey(block).path
+        horizontalBlock(block) { state: BlockState ->
+            models().getExistingFile(Terrartifacts.modLoc("$name${postfix(state.getValue(property))}"))
+        }
+    }
+
+    private fun <T> horizontalBlockWithProperty(
+        block: Block,
+        property: Property<T>
+    ) where T : Comparable<T>, T : StringRepresentable =
+        horizontalBlockWithProperty(block, property) { "_${it.serializedName}" }
 }
